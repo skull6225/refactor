8ebaeb314e64e1ebc51a00232cd3ad7aecd82d9f
skull6225 <skull6225@gmail.com>
2019-05-15 10:13:10 +0300

    refactor

---
+++ ap/src/main/java/ua/com/ap/UserAccountRepository.java
@@ -0,0 +1,30 @@
+package ua.com.ap;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import ua.com.ap.UserAccount;
+
+public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
+
+    /*
+     * Get user list by user name. Please note the format should be
+     * findBy<column_name>.
+     */ 
+    List<UserAccount> findByUsername(String username);
+
+    /*
+     * Get user list by user name and password. Please note the format should be
+     * findBy<column_name_1>And<column_name_2>.
+     */
+    List<UserAccount> findByUsernameAndPassword(String username, String password);
+
+    @Transactional
+    void deleteByUsernameAndPassword(String username, String password);
+
+    @Transactional
+    void deleteByUsername(String username);
+
+}
